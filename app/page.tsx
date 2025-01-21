677ef61dece60605a24cc237b28c628fdbafc473
Simon Gribert <simon.gribert@live.se>
2025-01-21 21:10:00 +0100

    feat: add oauth with google

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import { auth, signIn } from "@/auth";
 import { Button } from "antd";
 
-const Home = () => {
+const Home = async () => {
+  const session = await auth();
+
+  console.log(session);
+
   return (
     <div>
       <Button type="primary">Button</Button>
+
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google");
+        }}
+      >
+        <button type="submit">Signin with Google</button>
+      </form>
     </div>
   );
 };
