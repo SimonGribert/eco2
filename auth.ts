964901a5b3296a33105844e34973aad04fa8f9c6
Simon Gribert <simon.gribert@live.se>
2025-01-28 20:36:38 +0100

    fix: adjust middleware

--- auth.ts
+++ auth.ts
@@ -2,7 +2,6 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { prisma } from "./prisma/prisma";
-// import { Login } from "./prisma/lib/User";
 
 const privateRoutes = ["/"];
 const publicRoutes = ["/login"];
@@ -11,21 +10,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
   adapter: PrismaAdapter(prisma),
   callbacks: {
-    session: async ({ session }) => {
-      // try {
-      //   const dbUser = await Login(
-      //     session.user.email,
-      //     session.user?.name ?? "First Last",
-      //     session.user?.image ?? ""
-      //   );
-
-      //   session.user.id = dbUser.id.toString();
-      // } catch (err) {
-      //   console.error(err);
-      // }
-
-      return session;
-    },
     authorized: async ({ auth, request }) => {
       const path = request.nextUrl.pathname;
 
