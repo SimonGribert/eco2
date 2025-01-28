f04e8d9017f30d2fa2971b89233dbcf174ea1538
Simon Gribert <simon.gribert@live.se>
2025-01-28 21:55:03 +0100

    fix: add redirect url to tink link

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <Link
-        href={`https://link.tink.com/1.0/transactions/connect-accounts/?client_id=${process.env.TINK_CLIENT_ID}&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Ftink-callback&market=SE&locale=en_US`}
+        href={`https://link.tink.com/1.0/transactions/connect-accounts/?client_id=${process.env.TINK_CLIENT_ID}&redirect_uri=${process.env.TINK_REDIRECT_URL}&market=SE&locale=en_US`}
         target="_self"
       >
         Request Tink Access
